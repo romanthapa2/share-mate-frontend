f8962cef2c464d1dbc15e8a5596e9fe556e8281e
romanthapa2 <thaparoman970@gmail.com>
2025-04-08 11:01:07 +0545

    commented main file for testing pr agent

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Provider from './Provider/Context.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import Provider from './Provider/Context.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider>
-    <App />
-    </Provider>
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Provider>
+//     <App />
+//     </Provider>
+//   </StrictMode>,
+// )
